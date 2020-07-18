8d5e9c730126bc4a38134f85175058be9bd573af
David Walton <davidwalton6772@gmail.com>
2020-07-18 09:40:15 -0500

    updated with toggle but still not working

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -6,45 +6,79 @@ class GalleryItem extends Component {
     likes: [],
   };
 
-  displayText = () => {
-    if (!this.state.click) {
-      this.setState(
-        {
-          click: true,
-        },
-        () => {
-          console.log(this.state.click);
-        }
-      );
-    } else {
-      this.setState(
-        {
-          click: false,
-        },
-        () => {
-          console.log(this.state.click);
-        }
-      );
-    }
-  };
+  //   displayText = () => {
+  //     if (!this.state.click) {
+  //       this.setState(
+  //         {
+  //           click: true,
+  //         },
+  //         () => {
+  //           console.log(this.state.click);
+  //         }
+  //       );
+  //     } else {
+  //       this.setState(
+  //         {
+  //           click: false,
+  //         },
+  //         () => {
+  //           console.log(this.state.click);
+  //         }
+  //       );
+  //     }
+  //   };
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      on: !this.state.on,
+    });
+  }
   render() {
-    let el = this.props.imageList.map((image) => (
-      <div key={image.id}>
-        <img key={image.id} src={image.path} alt={image.description} />
-        <br></br>
-        <button onClick={this.showLikes}>LIKE IT!</button>
-        <p></p>
-      </div>
-    ));
-    if (this.state.click === true) {
-      el = this.props.imageList.map((image) => (
+    let status;
+    if (this.state.on) {
+      status = this.props.imageList.map((image) => (
+        <div key={image.id}>
+          <img key={image.id} src={image.path} alt={image.description} />
+          <br></br>
+          <button onClick={this.showLikes}>LIKE IT!</button>
+          <p></p>
+        </div>
+      ));
+    } else {
+      status = this.props.imageList.map((image) => (
         <div key={image.id}>
           <p key={image.id}>{image.description}</p>
         </div>
       ));
     }
-    return <div onClick={this.displayText}>HERE IS{el}</div>;
+    return (
+      <div>
+        <button onClick={this.handleClick.bind(this)}>Toggle</button>
+        {status}
+      </div>
+    );
   }
 }
+
+//   render() {
+//     let el = this.props.imageList.map((image) => (
+//       <div key={image.id}>
+//         <img key={image.id} src={image.path} alt={image.description} />
+//         <br></br>
+//         <button onClick={this.showLikes}>LIKE IT!</button>
+//         <p></p>
+//       </div>
+//     ));
+//     if (this.state.click === true) {
+//       el = this.props.imageList.map((image) => (
+//         <div key={image.id}>
+//           <p key={image.id}>{image.description}</p>
+//         </div>
+//       ));
+//     }
+//     return <div onClick={this.displayText}>HERE IS{el}</div>;
+//   }
+// }
 
 export default GalleryItem;
