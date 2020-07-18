4a1f84f0bb423a97e44caeb0068553236fc8c5b1
David Walton <davidwalton6772@gmail.com>
2020-07-18 15:20:49 -0500

    data transferred to db but not posting correctly

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem';
-import { Grid, Paper, Typography } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 
 class GalleryList extends Component {
   render() {
