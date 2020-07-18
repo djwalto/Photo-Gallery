549b6e4025a7bdc2da5e4e61fe82e1e942ce8d8f
David Walton <davidwalton6772@gmail.com>
2020-07-18 13:52:01 -0500

    base function complete, working get, post, and put routes

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import App from '../App/App';
 import GalleryItem from '../GalleryItem/GalleryItem';
+import { Grid, Paper, Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 
 class GalleryList extends Component {
   render() {
+    const htmlArray = this.props.imageList.map((item, index) => {
+      return <GalleryItem item={item} />;
+    });
     return (
       <div>
-        <h2>Images</h2>
-        <GalleryItem imageList={this.props.imageList} />
+        <ul>{htmlArray}</ul>
       </div>
     );
   }
